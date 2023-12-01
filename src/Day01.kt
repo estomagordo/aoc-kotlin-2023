0f3e44eefb203ff6189bacc08a682026ba4563a3
Christofer Ohlsson <christofer@miro.com>
2023-12-01 06:49:44 +0100

    Day 1a is done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,16 @@
 fun main() {
+    fun digval(line: String, pos: Int): Int? {
+        return line[pos].toString().toIntOrNull()
+    }
+
+    fun linedigval(line: String): Int {
+        val digs = line.indices.map { digval(line, it) }.filter { it != null }.map { it!! }
+
+        return 10 * digs.first() + digs.last()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { linedigval(it) }.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -8,8 +18,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
