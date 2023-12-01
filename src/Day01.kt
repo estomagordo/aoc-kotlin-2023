52bf5d58d55a1753beee945ecec476d8cb47fea7
Christofer Ohlsson <christofer@miro.com>
2023-12-01 11:37:00 +0100

    refactoring and basic validation

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,19 +4,21 @@ fun main() {
     }
 
     fun digOrWordVal(line: String, pos: Int): Int? {
-        if (digVal(line, pos) != null) {
-            return digVal(line, pos)
-        }
-
         val words = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
+        val fits = { word: String -> pos + word.length <= line.length }
+        val matches = { word: String -> line.substring(pos, pos + word.length) == word }
+        val isNotZero = { word: String -> words.indexOf(word) > 0 }
+
+        val goodMatch = { word: String -> fits(word) && matches(word) && isNotZero(word) }
+
         words.forEach {
-            if (pos + it.length <= line.length && line.substring(pos, pos + it.length) == it && words.indexOf(it) > 0) {
+            if (goodMatch(it)) {
                 return words.indexOf(it)
             }
         }
 
-        return null
+        return digVal(line, pos)
     }
 
     fun lineValue(line: String, evaluator: (String, Int) -> Int?): Int {
@@ -33,9 +35,11 @@ fun main() {
         return input.sumOf { lineValue(it) { line, pos -> digOrWordVal(line, pos) } }
     }
 
-    // test if implementation meets criteria from the description, like:
-    // val testInput = readInput("Day01_test")
-    // check(part1(testInput) == 1)
+    val testInputA = readInput("Day01_test_a")
+    check(part1(testInputA) == 142)
+
+    val testInputB = readInput("Day01_test_b")
+    check(part2(testInputB) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
