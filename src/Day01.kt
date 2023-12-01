372e01b45ef365f889c158210b1c5a4865db763d
Christofer Ohlsson <christofer@miro.com>
2023-12-01 07:31:06 +0100

    looking a bit better

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,11 +1,11 @@
 fun main() {
-    fun digval(line: String, pos: Int): Int? {
+    fun digVal(line: String, pos: Int): Int? {
         return line[pos].toString().toIntOrNull()
     }
 
-    fun digorwordval(line: String, pos: Int): Int? {
-        if (digval(line, pos) != null) {
-            return digval(line, pos)
+    fun digOrWordVal(line: String, pos: Int): Int? {
+        if (digVal(line, pos) != null) {
+            return digVal(line, pos)
         }
 
         val words = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
@@ -19,24 +19,18 @@ fun main() {
         return null
     }
 
-    fun linedigval(line: String): Int {
-        val digs = line.indices.map { digval(line, it) }.filter { it != null }.map { it!! }
-
-        return 10 * digs.first() + digs.last()
-    }
-
-    fun linedigwordval(line: String): Int {
-        val digs = line.indices.map { digorwordval(line, it) }.filter { it != null }.map { it!! }
+    fun lineValue(line: String, evaluator: (String, Int) -> Int?): Int {
+        val digs = line.indices.mapNotNull { evaluator(line, it) }.map { it }
 
         return 10 * digs.first() + digs.last()
     }
 
     fun part1(input: List<String>): Int {
-        return input.map { linedigval(it) }.sum()
+        return input.sumOf { lineValue(it) { line, pos -> digVal(line, pos) } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.map { linedigwordval(it) }.sum()
+        return input.sumOf { lineValue(it) { line, pos -> digOrWordVal(line, pos) } }
     }
 
     // test if implementation meets criteria from the description, like:
